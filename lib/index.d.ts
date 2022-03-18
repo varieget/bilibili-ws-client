c7f82c47d1e7ad5d91d5a1ad9819c12d09af0d00
varieget <varieget@gmail.com>
2022-03-18 20:00:33 +0800

    fix: 针对 EventEmitter 的 types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,59 @@
 /// <reference types="node" />
 import EventEmitter from 'events';
 import { TextEncoder, TextDecoder } from 'util';
-import type { Options, SubClient } from './subClient';
+import type { Ver, Op, Options, SubClient } from './subClient';
+interface Client {
+    on(event: 'close', listener: (this: Client) => void): this;
+    on(event: 'error', listener: (this: Client, err: Error) => void): this;
+    on(event: 'message', listener: (this: Client, body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+    }) => void): this;
+    on(event: 'open', listener: (this: Client, body: string) => void): this;
+    once(event: 'close', listener: (this: Client) => void): this;
+    once(event: 'error', listener: (this: Client, err: Error) => void): this;
+    once(event: 'message', listener: (this: Client, body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+    }) => void): this;
+    once(event: 'open', listener: (this: Client, body: string) => void): this;
+    off(event: 'close', listener: (this: Client) => void): this;
+    off(event: 'error', listener: (this: Client, err: Error) => void): this;
+    off(event: 'message', listener: (this: Client, body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+    }) => void): this;
+    off(event: 'open', listener: (this: Client, body: string) => void): this;
+    addListener(event: 'close', listener: (this: Client) => void): this;
+    addListener(event: 'error', listener: (this: Client, err: Error) => void): this;
+    addListener(event: 'message', listener: (this: Client, body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+    }) => void): this;
+    addListener(event: 'open', listener: (this: Client, body: string) => void): this;
+    removeListener(event: 'close', listener: (this: Client) => void): this;
+    removeListener(event: 'error', listener: (this: Client, err: Error) => void): this;
+    removeListener(event: 'message', listener: (this: Client, body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+    }) => void): this;
+    removeListener(event: 'open', listener: (this: Client, body: string) => void): this;
+}
 declare class Client extends EventEmitter implements SubClient {
     options: Options;
     textDecoder: TextDecoder;
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,106 @@ const verOffset = 6; // 协议版本
 const opOffset = 8; // 操作类型
 const seqOffset = 12; // 数据包头部
 
+interface Client {
+  // Events
+  on(event: 'close', listener: (this: Client) => void): this;
+  on(event: 'error', listener: (this: Client, err: Error) => void): this;
+  on(
+    event: 'message',
+    listener: (
+      this: Client,
+      body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+      }
+    ) => void
+  ): this;
+  on(event: 'open', listener: (this: Client, body: string) => void): this;
+
+  once(event: 'close', listener: (this: Client) => void): this;
+  once(event: 'error', listener: (this: Client, err: Error) => void): this;
+  once(
+    event: 'message',
+    listener: (
+      this: Client,
+      body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+      }
+    ) => void
+  ): this;
+  once(event: 'open', listener: (this: Client, body: string) => void): this;
+
+  off(event: 'close', listener: (this: Client) => void): this;
+  off(event: 'error', listener: (this: Client, err: Error) => void): this;
+  off(
+    event: 'message',
+    listener: (
+      this: Client,
+      body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+      }
+    ) => void
+  ): this;
+  off(event: 'open', listener: (this: Client, body: string) => void): this;
+
+  addListener(event: 'close', listener: (this: Client) => void): this;
+  addListener(
+    event: 'error',
+    listener: (this: Client, err: Error) => void
+  ): this;
+  addListener(
+    event: 'message',
+    listener: (
+      this: Client,
+      body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+      }
+    ) => void
+  ): this;
+  addListener(
+    event: 'open',
+    listener: (this: Client, body: string) => void
+  ): this;
+
+  removeListener(event: 'close', listener: (this: Client) => void): this;
+  removeListener(
+    event: 'error',
+    listener: (this: Client, err: Error) => void
+  ): this;
+  removeListener(
+    event: 'message',
+    listener: (
+      this: Client,
+      body: {
+        ver: Ver;
+        op: Op;
+        cmd?: unknown;
+        body: string | number;
+        ts: number;
+      }
+    ) => void
+  ): this;
+  removeListener(
+    event: 'open',
+    listener: (this: Client, body: string) => void
+  ): this;
+}
+
 class Client extends EventEmitter implements SubClient {
   options: Options;
 
