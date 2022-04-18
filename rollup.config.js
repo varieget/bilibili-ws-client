8e563ca8c094fbcc6cf4c8255965b3340a20295b
varieget <varieget@gmail.com>
2022-04-18 21:38:35 +0800

    chore(rollup): config file

--- rollup.config.js
+++ rollup.config.js
@@ -6,40 +6,24 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 
 const extensions = ['.ts', '.js', '.tsx', '.jsx'];
 
-export default defineConfig([
-  {
-    input: 'src/index.ts',
-    external: ['ws'],
-    output: {
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
       file: 'lib/index.cjs.js',
       format: 'cjs',
       exports: 'default',
-      globals: {
-        ws: 'WebSocket',
-      },
     },
-    plugins: [
-      nodeResolve({ extensions }),
-      eslint({ throwOnError: true }),
-      babel({ extensions, babelHelpers: 'bundled' }),
-      commonjs(),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    external: ['ws'],
-    output: {
+    {
       file: 'lib/index.esm.js',
       format: 'esm',
-      globals: {
-        ws: 'WebSocket',
-      },
     },
-    plugins: [
-      nodeResolve({ extensions }),
-      eslint({ throwOnError: true }),
-      babel({ extensions, babelHelpers: 'bundled' }),
-      commonjs(),
-    ],
-  },
-]);
+  ],
+  external: ['ws'],
+  plugins: [
+    nodeResolve({ extensions }),
+    eslint(),
+    babel({ extensions, babelHelpers: 'bundled' }),
+    commonjs(),
+  ],
+});
